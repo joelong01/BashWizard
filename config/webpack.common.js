1e4c283bc9981fd823a2e391023a5119fbb5618a
joelong01 <joelong@outlook.com>
2019-03-25 18:24:35 -0700

    syntax coloring does not work

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,30 +1,41 @@
-/*jshint esversion: 6 */ 
+/*jshint esversion: 6 */
 const path = require("path");
-
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const MONACO_DIR = path.resolve(__dirname, './node_modules/monaco-editor');
 module.exports = {
-  target: "electron-main",
-  entry: "./src/electron/main.ts",
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: [{
-          loader: "ts-loader",
-          options: {
-            compilerOptions: {
-              noEmit: false
-            }
-          }
-        }],
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".ts", ".js"]
-  },
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "../build")
-  }
+    plugins: [
+        // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+        new MonacoWebpackPlugin({languages: ['shell', 'json']})
+
+    ],
+    target: "electron-main",
+    entry: "./src/electron/main.ts",
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: [{
+                    loader: "ts-loader",
+                    options: {
+                        compilerOptions: {
+                            noEmit: false
+                        }
+                    }
+                }],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                include: MONACO_DIR,
+                use: ['style-loader', 'css-loader'],
+              }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "../build")
+    }
 };
